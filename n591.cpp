c55015fb648dff36ef985d8b8afc0a488252f4a5
posaune-0xa0 <tsposaune@gmail.com>
2018-06-27 05:59:11 +0900

    n591 solved

---
+++ n591.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    char t1, t2;
+    cin >> t1;
+    cin >> t2;
+
+    cout << "(" << t1 << t2 << t1 << ")/" << endl;
+    return 0;
+}
