b0b811d544938adc20dbf7c879b97821c15aa089
posaune-0xa0 <tsposaune@gmail.com>
2018-07-14 00:01:23 +0900

    n712 solved

---
+++ n712.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+ans = 0
+for i in range(N):
+    ans += input().count('W')
+print(ans)N, M = map(int, input().split())
+ans = 0
+for i in range(N):
+    ans += input().count('W')
+print(ans)
